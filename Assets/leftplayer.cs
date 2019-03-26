ef249dd4429ffca6c8f46680d46b6c9d4bd0a8be
User_Raum_174_31 <User_31@GeSa.local>
2019-03-26 15:11:49 +0100

    TarekistGay

---
+++ Assets/leftplayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftplayer : MonoBehaviour
+{
+    public float thrust;
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+           _rb.AddForce(Vector3.up * thrust * Input.GetAxis("Vertical"));
+        
+    }
+}
